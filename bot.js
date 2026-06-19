8c9adef46f43b3ce7bb05d982a257ed78cd1abb7
midnightShadxw <mavadoinshadow@gmail.com>
2026-06-19 09:41:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,15 @@
 const { Client, GatewayIntentBits } = require('discord.js')
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] })
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
 
 client.once('ready', () => {
     console.log('bot is online')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return
+    if (message.content === 'how are you') {
+        message.reply('i am doing great thank you for asking')
+    }
 })
 
 client.login(process.env.DISCORD_TOKEN)
